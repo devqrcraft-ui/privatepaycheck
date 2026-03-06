573d098fadbf3d697b798aabca7933d05d729fb8
Ruslan <ruslan@compressto20kb.com>
2026-03-06 22:27:30 +0200

    fix: add redirects for 404 URLs hr/week/hourly-paycheck-calculator states

--- next.config.js
+++ next.config.js
@@ -1,3 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    const states = [
+      'alabama','alaska','arizona','arkansas','california','colorado',
+      'connecticut','delaware','florida','georgia','hawaii','idaho',
+      'illinois','indiana','iowa','kansas','kentucky','louisiana',
+      'maine','maryland','massachusetts','michigan','minnesota',
+      'mississippi','missouri','montana','nebraska','nevada',
+      'new-hampshire','new-jersey','new-mexico','new-york',
+      'north-carolina','north-dakota','ohio','oklahoma','oregon',
+      'pennsylvania','rhode-island','south-carolina','south-dakota',
+      'tennessee','texas','utah','vermont','virginia','washington',
+      'west-virginia','wisconsin','wyoming'
+    ];
+    const stateRedirects = states.map(s => ({
+      source: `/hourly-paycheck-calculator/${s}`,
+      destination: '/hourly-paycheck-calculator',
+      permanent: true,
+    }));
+    return [
+      { source: '/hr', destination: '/hourly-paycheck-calculator', permanent: true },
+      { source: '/week', destination: '/hourly-paycheck-calculator', permanent: true },
+      ...stateRedirects,
+    ];
+  },
+};
 module.exports = nextConfig;
